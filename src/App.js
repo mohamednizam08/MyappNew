0410aefaa95103b3b1577173dde78fb171ef4631
Mohamed Nizam <mohamednizam@Mohameds-MBP.realtek>
2023-10-31 18:51:22 +0800

    Git Feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-          Started coding now
+          Started coding now added git 
         </p>
         <a
           className="App-link"
